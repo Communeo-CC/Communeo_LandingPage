6519146f285202ea146e8dd952e4fbb67ae5689f
ashen3434 <visalvithanage376@gmail.com>
2025-01-19 21:11:39 +0530

    Folders for team members

---
+++ src/components/Thamindu/FluidCursorDemo.jsx
@@ -0,0 +1,14 @@
+"use client";
+
+import FluidCursor from "../ui/fluid-cursor";
+
+const FluidCursorDemo = () => {
+
+  return (
+    <>
+
+      <FluidCursor />
+    </>
+  );
+};
+export default FluidCursorDemo;
