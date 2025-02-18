59bf98f32ea047873568f772795f8241eae7d5a6
Visal Vithanage <visalvithanage376@gmail.com>
2025-02-19 05:08:16 +0530

    Revert "Signup"
    This reverts commit 6e84f75483f1b75dacb817b8d2262e96fda3b7ff.

--- src/SignUp.jsx
+++ src/SignUp.jsx
@@ -1,28 +1,34 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
-// import Benefits from "./components/Thamindu/Benefits";
-// import Collaboration from "./components/Thamindu/Collaboration";
+import Benefits from "./components/Thamindu/Benefits";
+import Collaboration from "./components/Thamindu/Collaboration";
 import Footer from "./components/Thamindu/Footer";
 import Header from "./components/Thamindu/Header";
 
-// import Pricing from "./components/Thamindu/Pricing";
-// import Roadmap from "./components/Thamindu/Roadmap";
-// import Services from "./components/Thamindu/Services";
+import Pricing from "./components/Thamindu/Pricing";
+import Roadmap from "./components/Thamindu/Roadmap";
+import Services from "./components/Thamindu/Services";
 
-// import FUIFeatureSectionWithCards from "./components/Thamindu/FUIFeatureSectionWithCards";
-// // import GradualSpacing from "./components/ui/GradualSpacing";
-// import FluidCursorDemo from "../src/components/Thamindu/FluidCursorDemo"
-import { SignUpForm } from "./components/Thamindu/SignUpForm";
+import FUIFeatureSectionWithCards from "./components/Thamindu/FUIFeatureSectionWithCards";
+import GradualSpacing from "./components/ui/GradualSpacing";
+import FluidCursorDemo from "../src/components/Thamindu/FluidCursorDemo"
 
 
 
 const App = () => {
   return (
     <>
+      <FluidCursorDemo />
       
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Header />
-        <SignUpForm/>
-
+        <GradualSpacing/>
+        {/* SpotlightPreview is a custom component */}
+        <Benefits />
+        <FUIFeatureSectionWithCards />
+        <Collaboration />
+        <Services />
+        <Pricing />
+        <Roadmap />
         <Footer />
       </div>
 
@@ -33,4 +39,3 @@ const App = () => {
 };
 
 export default App;
-
